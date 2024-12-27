f8a1ca157f8802c0962f05d2d75febda9883231d
pankaj singh koshyari <pankajsinghkoshyari@pankajsinghkoshyari.local>
2024-12-27 12:02:53 +0530

    expe

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,31 @@
-import { OrbitControls } from "@react-three/drei";
-import { Office } from "./Office";
-// import { Overlay } from "./Overlay";
+// import { OrbitControls } from "@react-three/drei";
+// import { Office } from "./Office";
+// // import { Overlay } from "./Overlay";
 
-export const Experience = () => {
+// export const Experience = () => {
+//   return (
+//     <>
+//       <ambientLight intensity={1} />
+//       <OrbitControls />
+//       {/* <ScrollControls pages={3} damping={0.25}> */}
+//         {/* <Overlay /> */}
+//         <Office />
+//       {/* </ScrollControls> */}
+//     </>
+//   );
+// };
+
+import React from 'react'
+import Office from './Office'
+import { OrbitControls } from '@react-three/drei'
+
+const Experience = () => {
   return (
     <>
-      <ambientLight intensity={1} />
-      <OrbitControls />
-      {/* <ScrollControls pages={3} damping={0.25}> */}
-        {/* <Overlay /> */}
-        <Office />
-      {/* </ScrollControls> */}
+    <OrbitControls />
+    <Office />
     </>
-  );
-};
+  )
+}
+
+export default Experience
