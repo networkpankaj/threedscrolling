da6c3bf38cb994caff414da3615816e8fbdce63f
pankaj singh koshyari <pankajsinghkoshyari@pankajsinghkoshyari.local>
2024-12-27 10:21:07 +0530

    initialise

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Canvas } from '@react-three/fiber'
+import React from 'react'
+import Experience from './components/Experience'
+
+const App = () => {
+  return (
+    <Canvas>
+      <Experience />
+    </Canvas>
+  )
+}
+
+export default App
